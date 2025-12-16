7714bb39a2c5ae272320878a09a01666f8acfdc2
Quirin176 <voquangqui176@gmail.com>
2025-12-16 10:48:03 +0700

    Updated 15_12_2025

--- WebApp_API/WebApp_API/obj/Debug/net10.0/WebApp_API.AssemblyInfo.cs
+++ WebApp_API/WebApp_API/obj/Debug/net10.0/WebApp_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+973cb9b1494955d8cffc9925c211033eaf5e6f80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da593c1242c2d39090a2104be907e552873b51fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
